0d2db47aa096515441d88b409461afd36407e714
Robson Amorim <robson.amorim@lambda3.com.br>
2018-11-13 18:22:32 -0200

    initial commit

---
+++ App/App/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _texto;
+
+        public string Texto
+        {
+            get { return _texto; }
+            set { _texto = value; OnPropertyChanged(); }
+        }
+
+        public MainPageViewModel()
+        {
+            Texto = "OOOOI";
+        }
+    }
+}
